5ad7dcc1200c2605028091ec4d61e23d6886f818
mdr-datascience <mdr.datascience@gmail.com>
2023-10-09 14:16:44 +0100

    commit tutorial 1

--- Python_Tkinter/tkinter-tutorial-0.py
+++ Python_Tkinter/tkinter-tutorial-0.py
@@ -4,4 +4,17 @@ import tkinter
 import _tkinter
 
 # Run test routine
-tkinter._test()
+#tkinter._test()
+
+# Import tkinter module
+from tkinter import *
+from tkinter import ttk
+
+# Create top-level window
+root = Tk()
+
+# Create first button
+button = ttk.Button(root, text = 'Click Me')
+
+# To insert a button we need to call the placement method
+button.pack()
